6e9d462c202d4e78de7f921e3bb982ee072be0af
Ishika Raj <ishika20_ug@cse.nits.ac.in>
2024-12-08 16:27:18 +0530

    added pages

--- src/pages/MarathonClasses.js
+++ src/pages/MarathonClasses.js
@@ -11,14 +11,14 @@ const MarathonClasses = () => {
   };
 
   const companyDocs = [
-    { name: "TCS", link: "https://example.com/tcs-doc" },
-    { name: "TCS NQT", link: "https://example.com/tcs-nqt-doc" },
-    { name: "WIPRO", link: "https://example.com/wipro-doc" },
-    { name: "Tech Mahindra", link: "https://example.com/tech-mahindra-doc" },
-    { name: "Accenture", link: "https://example.com/accenture-doc" },
-    { name: "Capgemini", link: "https://example.com/capgemini-doc" },
-    { name: "Cognizant", link: "https://example.com/cognizant-doc" },
-    { name: "TCS CodeVita", link: "https://example.com/tcs-codevita-doc" },
+    { name: "TCS", link: "https://docs.google.com/document/d/14wk4V0X0CbYZjSiEks2hrkmcIBRUBdwSDOQTSxfyEZI/edit?tab=t.0" },
+    { name: "TCS NQT", link: "https://docs.google.com/document/d/187up3ub6fP9ePT973N8pnFpGfY0aoxuKEQk7Rwptnjw/edit?tab=t.0" },
+    { name: "WIPRO", link: "https://docs.google.com/document/d/1IIoC3AAbyGw1GrtP1LLuXBy1q2jxDbgwQtCxd6XIh6w/edit?tab=t.0" },
+    { name: "Tech Mahindra", link: "https://docs.google.com/document/d/1fiyZRtd8ZRtFO1kgNBjbgr8Dva7i6tmwxdDXFn6RM9w/edit?tab=t.0" },
+    { name: "Accenture", link: "https://docs.google.com/document/d/1XHBCgvHNLH8GkL4G8lkyVsQbPm-r290ykPSNSt7kIJU/edit?tab=t.0" },
+    { name: "Capgemini", link: "https://docs.google.com/document/d/1MJOnI_goA5m9sG7mMS7h6BigE8iloPuMWP-7-5ilKQ0/edit?tab=t.0" },
+    { name: "Cognizant", link: "https://docs.google.com/document/d/1BfVQhNb_ICQfxrv-2yL091oDtGShJywZ1MvoKTVRJpQ/edit?tab=t.0" },
+    { name: "Infosys", link: "https://docs.google.com/document/d/1YlIjdy1uZR1RuhIYU3oz9YXGy-oPfC8fZTox6PYJBo4/edit?tab=t.0#heading=h.hcwyy4lc8uv6" },
   ];
 
 
