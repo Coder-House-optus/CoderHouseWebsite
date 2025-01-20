2ffa20fdb2cb5da0db66f5339359e564331f8448
Aasthapurey <aasthapurey@gmail.com>
2025-01-20 19:14:07 +0530

    final commit

--- src/pages/SchoolProgram.js
+++ src/pages/SchoolProgram.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
 import './SchoolProgram.css';
 import { useNavigate } from 'react-router-dom';
 
 const SchoolProgram = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   // Function to handle navigation for buttons below images
   const handleImageButtonClick = (page) => {
     navigate(`/${page}`);
@@ -24,36 +26,33 @@ const SchoolProgram = () => {
   ];
 
   return (
-  <div className="page-container">
-  {/* Back Button */}
-  <button className="back-button1" onClick={() => navigate('/Home')}>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <line x1="19" y1="12" x2="5" y2="12"></line>
-          <polyline points="12 19 5 12 12 5"></polyline>
-        </svg>
-      </button>
-    <div className="background-section">
-      <div className="content-wrapper">
-        <h1 className="main-title">We Are Coming Very Soon!</h1>
-        <div className="projects-grid">
-          {projects.map((project, index) => (
-            <div key={index} className="project-card">
-              <div className="card-image-container">
-                <img
-                  src={project.imageUrl}
-                  alt={project.title}
-                  className="card-image"
-                />
-              </div>
-              <div className="card-content">
-                <h3 className="card-title">{project.title}</h3>
-              </div>
+    <>
+      <Navigation />
+      <div className="page-containerforschool">
+        <div className="background-sectionforschool">
+          <div className="content-wrapperforschool">
+            <h1 className="main-titleforschool">We Are Coming Very Soon!</h1>
+            <div className="projects-gridforschool">
+              {projects.map((project, index) => (
+                <div key={index} className="project-cardforschool">
+                  <div className="card-image-containerforschool">
+                    <img
+                      src={project.imageUrl}
+                      alt={project.title}
+                      className="card-imageforschool"
+                    />
+                  </div>
+                  <div className="card-contentforschool">
+                    <h3 className="card-titleforschool">{project.title}</h3>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+      <Footer />
+    </>
   );
 };
 
